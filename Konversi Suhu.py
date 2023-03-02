56f7cd961c384574b0c1987642b70ee34c2e4f9f
HayatunNupus15 <126741819+HayatunNupus15@users.noreply.github.com>
2023-03-02 10:15:09 +0700

    Hayatun Nupus_21346011_Praktikukm AI_Widya Darwin S.pd. M.Pd.T

---
+++ Konversi Suhu.py
@@ -0,0 +1,10 @@
+# Program konversi suhu dari Celcius ke Fahrenheit
+
+# Meminta input dari pengguna
+celsius = float(input("Masukkan suhu dalam Celcius: "))
+
+# Konversi suhu dari Celcius ke Fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+
+# Menampilkan hasil konversi suhu
+print(celsius, "derajat Celcius sama dengan", fahrenheit, "derajat Fahrenheit.")
